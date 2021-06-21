3ac43e72a5ce2be6e0d2f193c8661dbde70a1714
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-06-21 15:15:25 -0400

    initilialize flickity

--- footer.php
+++ footer.php
@@ -67,7 +67,20 @@
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
+<script>
+	var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
 
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+</script>
 <?php wp_footer(); ?>
 
 </body>
