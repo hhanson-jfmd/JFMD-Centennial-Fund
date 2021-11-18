9513b3c50c3442c41b0ce21d8bc4c7006cb5039c
brian <fritz@jfmd.org>
2021-11-18 15:52:03 -0500

    okay wtf, let's just copy it I guess

--- single-videos.php
+++ single-videos.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Template Name: Video Story (Single Post)
- * Template Post Type: post, page
+ * Template Name Posts: Video Story (Single Post)
  * The template for displaying all single posts
+ *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package WordPress
  * @subpackage Twenty_Twenty_One
  * @since Twenty Twenty-One 1.0
  */
-
 
 get_header();
 
@@ -18,15 +17,37 @@ while ( have_posts() ) :
 	the_post();
 
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="video entry-header">
-        <h1 class="entry-title">Centennial Stories</h1>
-        <div class="video-container"><iframe src="https://player.vimeo.com/video/<?php the_field ('vimeo_embed'); ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="75 Years of Women&amp;#039;s Philanthropy"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-        <?php the_content(); ?>
+	<header class="video entry-header default-max-width post-info">
+		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
+		<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/<?php the_field ('vimeo_embed'); ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="75 Years of Women&amp;#039;s Philanthropy"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+        <?php echo get_the_category_list(); ?>
+        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
+		<h5 class="entry-author">By <?php the_author(); ?></h5>
+        <h5 class="entry-date"><?php echo get_the_date(); ?></h5>
 		
 	</header><!-- .entry-header -->
+
+	<div class="video entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: Page number. */
+				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer default-max-width">
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer><!-- .entry-footer -->
 
 	<?php if ( ! is_singular( 'attachment' ) ) : ?>
 		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
@@ -35,6 +56,30 @@ while ( have_posts() ) :
 </article><!-- #post-<?php the_ID(); ?> -->
 <?php	
 
+	if ( is_attachment() ) {
+		// Parent post navigation.
+		the_post_navigation(
+			array(
+				/* translators: %s: Parent post link. */
+				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone' ), '%title' ),
+			)
+		);
+	}
+
+
+	// Previous/next post navigation.
+	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
+	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
+
+	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
+	$twentytwentyone_previous_label = esc_html__( 'Previous post', 'twentytwentyone' );
+
+	the_post_navigation(
+		array(
+			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
+			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
+		)
+	);
 endwhile; // End of the loop.
 ?>
 
