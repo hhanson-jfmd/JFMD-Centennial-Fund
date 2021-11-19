10f7d4677dd03e3a80a6187b444ee429c72743e6
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-11-19 12:49:27 -0500

    magic words I guess

--- single-videos.php
+++ single-videos.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Template Name Posts: Video Story (Single Post)
+ * Template Post Type: post, page
  * The template for displaying all single posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
