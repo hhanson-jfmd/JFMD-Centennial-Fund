06eaeea1429934f65df27ace8a7655db232730ad
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2021-11-19 12:46:40 -0500

    video page adjstments

--- single-videos.php
+++ single-videos.php
@@ -21,9 +21,10 @@ while ( have_posts() ) :
 
 	<header class="video entry-header default-max-width post-info">
 		<?php // - Default Thumbnail call twenty_twenty_one_post_thumbnail(); ?>
-		<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/<?php the_field ('vimeo_embed'); ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="75 Years of Women&amp;#039;s Philanthropy"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+		<h1 class="entry-title">Centennial Stories</h1>
+		<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/<?php the_field ('vimeo_embed'); ?>?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
         <?php echo get_the_category_list(); ?>
-        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <?php the_title( '<h3 class="">', '</h3>' ); ?>
 		<h4 class="entry-excerpt"><?php the_excerpt(); ?></h4>
 		<h5 class="entry-author">By <?php the_author(); ?></h5>
         <h5 class="entry-date"><?php echo get_the_date(); ?></h5>
